b1151c1bde6733c00a8a8ec9375602d85e45a917
pablocomin07 <pablocomin07@gamil.com>
2023-03-02 18:33:58 -0300

    Updates

---
+++ precache-manifest.82b71fc0e794e0e128b4e2a97c4bc37e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e3b96f14d0e84111eb9",
+    "url": "/Deploy_Frontend_PING8/static/css/main.5168d50d.chunk.css"
+  },
+  {
+    "revision": "5e3b96f14d0e84111eb9",
+    "url": "/Deploy_Frontend_PING8/static/js/main.5e3b96f1.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/Deploy_Frontend_PING8/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "c47f4c0a8d44924e8b0c",
+    "url": "/Deploy_Frontend_PING8/static/js/2.c47f4c0a.chunk.js"
+  },
+  {
+    "revision": "90b83cc811e72172173b",
+    "url": "/Deploy_Frontend_PING8/static/js/runtime~main.90b83cc8.js"
+  },
+  {
+    "revision": "b9004c789329957987b8283b4b3caf43",
+    "url": "/Deploy_Frontend_PING8/static/media/logo.b9004c78.jpg"
+  },
+  {
+    "revision": "03ebce603be35e93af566bbc4be36166",
+    "url": "/Deploy_Frontend_PING8/static/media/imagen8_Chalten.03ebce60.jpg"
+  },
+  {
+    "revision": "0aa7baeeb34012e1397b6dc9ae8de1fa",
+    "url": "/Deploy_Frontend_PING8/static/media/imagen9_Chalten.0aa7baee.jpg"
+  },
+  {
+    "revision": "1869171d077f3e988d840d84f31e3c49",
+    "url": "/Deploy_Frontend_PING8/static/media/imagen5_Chalten.1869171d.jpg"
+  },
+  {
+    "revision": "cb96ad2005898a956e6e283b28255630",
+    "url": "/Deploy_Frontend_PING8/static/media/imagen7_Chalten.cb96ad20.jpg"
+  },
+  {
+    "revision": "20bbe737ea2047100dcf631a89f71e7f",
+    "url": "/Deploy_Frontend_PING8/static/media/imagen3_Chalten.20bbe737.jpg"
+  },
+  {
+    "revision": "c2d5be91027f9de1ba5cdd839affc373",
+    "url": "/Deploy_Frontend_PING8/static/media/imagen6_Chalten.c2d5be91.jpg"
+  },
+  {
+    "revision": "21f0a67e571f7013e55d860642a3bc05",
+    "url": "/Deploy_Frontend_PING8/static/media/imagen4_Chalten.21f0a67e.jpg"
+  },
+  {
+    "revision": "55f2c289c71bec67c6792adc13f5c0dc",
+    "url": "/Deploy_Frontend_PING8/index.html"
+  }
+];
